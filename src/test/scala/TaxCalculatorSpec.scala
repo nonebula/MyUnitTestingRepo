6f56d3dc767f56ed021442349c60bceaa5d59f08
Joe Higgs <joe.higgs@mercator.group>
2024-07-11 08:34:28 +0100

    started task

--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -1,4 +1,3 @@
-import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
 
 class TaxCalculatorSpec extends AnyWordSpec {
@@ -13,6 +12,62 @@ class TaxCalculatorSpec extends AnyWordSpec {
 
         assert(result == 0)
       }
+      "the income is within the basic rate limit" in {
+        val result: Double = taxCalculator.calculateTax(20000)
+
+        assert(result == 2000)
+      }
+      "the income is within the higher rate limit"
+    }
+  }
+
+  //Second Test
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return whether the payee is a higher rate tax payer" when {
+      "the income is above the higher rate tax limit" in {
+        val result: Double = taxCalculator.isHigherRateTaxpayer(125000)
+
+        assert(result > 0)
+      }
+    }
+  }
+
+  // A method which can tell you if someone is a higher rate taxpayer
+  def isHigherRateTaxpayer(income: Double): Boolean = {
+    ???
+  }
+
+
+  //Third Test
+  "TaxCalculator.calculateTax" should {
+    "return a string with the income limit of the payee's current tax band" when {
+      "the income is below the personal tax limit" in {
+        val result: Double = taxCalculator.calculateTax(5000)
+
+        assert(result == 0)
+      }
     }
   }
 }
+
+// A method that will return a string with the income limit of their current tax band.
+// The return will also be formatted, E.g: "£12,500" or "No limit"
+def formattedCurrentTaxAllowance(income: Double): String = {
+  ???
+}
+
+
+
+//class TaxCalculator {
+//
+//  // Tax bands (simplified to make testing a bit easier)
+//  private val personalAllowance: Int = 10000
+//  private val basicRateLimit: Int = 50000
+//  private val higherRateLimit: Int = 125000
+//
+//  // Tax rates
+//  private val personalAllowanceRate: Double = 0
+//  private val basicRate: Double = 0.2
+//  private val higherRate: Double = 0.4
+//  private val additionalRate: Double = 0.45
+
